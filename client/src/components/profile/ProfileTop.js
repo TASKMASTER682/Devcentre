657756803e3960d6e471cc39f7bdd0dd2b5f4e46
TASKMASTER682 <sayedanwarulhaq@gmail.com>
2020-07-19 21:46:18 +0530

    github rendering added

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -11,7 +11,10 @@ const ProfileTop = ({
         user:{name,avatar}
     }
 }) => {
+    
     return (
+
+
         <div class="profile-top bg-primary p-2">
         <img
           class="round-img my-1"
